cabde9db97be461218294f2a08db610b29b3fc90
Derick Park <derickmpark@gmail.com>
2020-04-11 16:04:35 -0700

    Balanced Binary Tree

--- #5 Trees and Graphs/BalancedBinaryTree.js
+++ #5 Trees and Graphs/BalancedBinaryTree.js
@@ -1,34 +1,6 @@
 // Write a function to see if a binary tree ↴ is "superbalanced" (a new tree property we just made up).
-
 // A tree is "superbalanced" if the difference between the depths of any two leaf nodes ↴ is no greater than one.
 
-class Tree {
-  constructor(node) {
-    this.value = node;
-    this.right = null;
-    this.left = null;
-  }
-  insert(value) {
-    var current = this;
-    while (current) {
-      if (value < current.value) {
-        if (current.left) {
-          current = current.left;
-        } else {
-          current.left = new Tree(value);
-          break;
-        }
-      } else {
-        if (current.right) {
-          current = current.left;
-        } else {
-          current.left = new Tree(value);
-          break;
-        }
-      }
-    }
-  }
-}
 
 // Time: O(n)
 // Space: O(n)
@@ -36,11 +8,9 @@ function isBalanced(treeRoot) {
   if (!treeRoot) {
     return true;
   }
-  
   const depths = [];
   const nodes = [];
   nodes.push([treeRoot, 0]);
-  
   while (nodes.length) {
     const nodePair = nodes.pop();
     const node = nodePair[0];
